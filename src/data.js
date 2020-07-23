19d5bea741f0620840cab9056128ed6d8103029c
Panotsingsith <Toe@gmail.com>
2020-07-23 12:23:46 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            id: 1,
+            name: 'เสื้อ',
+            brand: 'Nike',
+            price: 3000,
+            rating: 4.5,
+            numStock: 100,
+            numReview: 10,
+            image:'/images/p1.jpg'
+        },
+        {
+            id: 2,
+            name: 'กางเกง',
+            brand: 'Adidas',
+            price: 1500,
+            rating: 4.5,
+            numStock: 80,
+            numReview: 30,
+            image:'/images/p2.jpg'
+        },
+        {
+            id: 3,
+            name: 'รองเท้า',
+            brand: 'Nike',
+            price: 3000,
+            rating: 4.5,
+            numStock: 20,
+            numReview: 30,
+            image:'/images/p3.jpg'
+        },
+        {
+            id: 2,
+            name: 'กางเกง',
+            brand: 'Nike',
+            price: 4500,
+            rating: 4.5,
+            numStock: 10,
+            numReview: 30,
+            image:'/images/p4.jpg'
+        }
+    ]
+}
