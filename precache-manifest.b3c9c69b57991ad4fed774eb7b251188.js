42d8efd20946569afca643fb47befc8d3946a4f2
Panotsingsith <Toe@gmail.com>
2020-07-23 12:38:30 +0700

    Updates

---
+++ precache-manifest.b3c9c69b57991ad4fed774eb7b251188.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99aca19673f839ee8c9001ca44f74eb2",
+    "url": "/basic-react2/index.html"
+  },
+  {
+    "revision": "c467e527f09a785400d8",
+    "url": "/basic-react2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9887b699584565defe5f",
+    "url": "/basic-react2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c467e527f09a785400d8",
+    "url": "/basic-react2/static/js/2.c46bd2d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/basic-react2/static/js/2.c46bd2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9887b699584565defe5f",
+    "url": "/basic-react2/static/js/main.7c6eb3a4.chunk.js"
+  },
+  {
+    "revision": "7afb22598af96d5d2805",
+    "url": "/basic-react2/static/js/runtime-main.41b44986.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic-react2/static/media/logo.5d5d9eef.svg"
+  }
+]);
